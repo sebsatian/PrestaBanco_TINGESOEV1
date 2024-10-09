1acb2e819e77a2fd4808d4b5c6fafbf769448599
unknown <sebastian.paillao@usach.cl>
2024-10-09 16:05:52 -0300

    frontend initialized using npm

---
+++ PrestaBancoTingesoEv1/BackendPrestaBanco/src/main/java/com/example/prestabanco/services/ClientService.java
@@ -0,0 +1,8 @@
+package com.example.prestabanco.services;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Service;
+public class ClientService {
+
+
+}
